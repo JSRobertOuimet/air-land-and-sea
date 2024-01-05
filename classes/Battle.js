037e61743539cc31044796adc6c6b7925d5ce7d8
JSRobertOuimet <jsrobertouimet@gmail.com>
2024-01-04 22:12:05 -0500

    Displayed theater boards and cards.

--- classes/Battle.js
+++ classes/Battle.js
@@ -1,14 +1,14 @@
-import Theater from "./Theater.js";
-import Card from "./Card.js";
-
 export default class Battle {
-    constructor(playerOne, playerTwo, theaterBoards, cards) {
+    constructor(playerOne, playerTwo, theaterBoards, cards, ui) {
         this.theaters = this.#shuffleCards(theaterBoards);
-        console.log(`🎲 The theater boards have been shuffled and appear in this order: ${this.theaters[0].name}, ${this.theaters[1].name}, and ${this.theaters[2].name}.`);
+        console.log(`${playerOne.name} has shuffled and dealt the theater boards.`);
+        this.#displayTheaterBoards(ui, this.theaters);
 
         this.cards = this.#shuffleCards(cards);
         this.#dealCards(this.cards, playerOne, playerTwo);
-        console.log("🎲 The cards have been shuffled and dealt to both players.");
+        this.dealtCards = playerOne.hand.concat(playerTwo.hand);
+        this.#displayCards(ui, this.dealtCards);
+        console.log(`${playerOne.name} has shuffled and dealt the cards.`);
     }
 
     #shuffleCards(cards) {
@@ -26,7 +26,7 @@ export default class Battle {
 
     #dealCards(cards, playerOne, playerTwo) {
         for(let i = 0; i < 12; i++) {
-            if(i % 2 == 0) {
+            if(i % 2 !== 0) {
                 playerOne.hand.push(cards[i]);
             } else {
                 playerTwo.hand.push(cards[i]);
@@ -36,5 +36,82 @@ export default class Battle {
 
     rotateCards(cards) {
 
+    }
+
+    #displayTheaterBoards(ui, theaters) {
+        const theaterBoardsEl = document.querySelector("#theater-boards");
+
+        theaters.forEach(theater => {
+            const theaterEl = ui.createElement("div");
+            const nameEl = ui.createElement("div");
+
+            theaterEl.classList.add("theater-board");
+
+            nameEl.innerHTML = `&ndash;${theater.name}&ndash;`;
+            nameEl.classList.add("name");
+
+            switch(theater.name) {
+                case "Air":
+                    theaterEl.classList.add("air");
+                    break;
+                case "Land":
+                    theaterEl.classList.add("land");
+                    break;
+                case "Sea":
+                    theaterEl.classList.add("sea");
+                    break;
+            }
+
+            theaterEl.append(nameEl);
+
+            ui.displayElement(theaterEl, theaterBoardsEl);
+        });
+    }
+
+    #displayCards(ui, cards) {
+        const playerOne = document.querySelector("#player-one");
+        const playerTwo = document.querySelector("#player-two");
+
+        cards.forEach((card, i) => {
+            const cardEl = ui.createElement("div");
+            const strengthEl = ui.createElement("div");
+            const tacticalAbilityEl = ui.createElement("div");
+            const descriptionEl = ui.createElement("small");
+
+            cardEl.setAttribute("id", card.id);
+            cardEl.classList.add("card");
+            cardEl.addEventListener("click", e => {
+                // prompt("Deploy, improvise, or withdraw?");
+            });
+
+            strengthEl.innerHTML = card.strength;
+            strengthEl.classList.add("strength");
+
+            tacticalAbilityEl.classList.add("tactical-ability");
+            tacticalAbilityEl.innerHTML = card.tacticalAbility;
+            
+            descriptionEl.innerHTML = card.description;
+            descriptionEl.classList.add("description");
+
+            switch(card.theater) {
+                case "Air":
+                    cardEl.classList.add("air");
+                    break;
+                case "Land":
+                    cardEl.classList.add("land");
+                    break;
+                case "Sea":
+                    cardEl.classList.add("sea");
+                    break;
+            }
+            
+            cardEl.append(strengthEl, tacticalAbilityEl, descriptionEl);
+            
+            if(i % 2 !== 0) {
+                ui.displayElement(cardEl, playerOne);
+            } else {
+                ui.displayElement(cardEl, playerTwo);
+            }
+        });
     }
 }
