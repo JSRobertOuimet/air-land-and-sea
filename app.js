c147964578c2074387c664e2033e802dc0f0ecd0
JSRobertOuimet <jsrobertouimet@gmail.com>
2024-01-04 22:06:54 -0500

    Removed emojis from strings; commented out manual player actions.

--- app.js
+++ app.js
@@ -2,27 +2,27 @@ import Player from "./classes/Player.js";
 import Game from "./classes/Game.js";
 
 // GAME SETUP //
-const playerOne = new Player("👩🏻 Allison");
-const playerTwo = new Player("🧑🏻 Jean-Simon");
+const playerOne = new Player("Allison");
+const playerTwo = new Player("JS");
 const game = new Game(playerOne, playerTwo);
 
-// BATTLE 1 //
-playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[0]);
-playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[0]);
-playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[1]);
-playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[1]);
-playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[2]);
-playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[2]);
-playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[0]);
-playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[0]);
-playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[1]);
-playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[1]);
-playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[2]);
-playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[2]);
+// // BATTLE 1 //
+// playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[0]);
+// playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[0]);
+// playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[1]);
+// playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[1]);
+// playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[2]);
+// playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[2]);
+// playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[0]);
+// playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[0]);
+// playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[1]);
+// playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[1]);
+// playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[2]);
+// playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[2]);
 
-console.table(game.battles[0].theaters[0].playerOneCards);
-console.table(game.battles[0].theaters[0].playerTwoCards);
-console.table(game.battles[0].theaters[1].playerOneCards);
-console.table(game.battles[0].theaters[1].playerTwoCards);
-console.table(game.battles[0].theaters[2].playerOneCards);
-console.table(game.battles[0].theaters[2].playerTwoCards);
+// console.table(game.battles[0].theaters[0].playerOneCards);
+// console.table(game.battles[0].theaters[0].playerTwoCards);
+// console.table(game.battles[0].theaters[1].playerOneCards);
+// console.table(game.battles[0].theaters[1].playerTwoCards);
+// console.table(game.battles[0].theaters[2].playerOneCards);
+// console.table(game.battles[0].theaters[2].playerTwoCards);
