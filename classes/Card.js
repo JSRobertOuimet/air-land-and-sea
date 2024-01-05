b493509f6e43dd978d01d4894a0cc3fa18033612
JSRobertOuimet <jsrobertouimet@gmail.com>
2024-01-04 22:09:28 -0500

    Added card description and type of tactical ability (instant or ongoing); renamed type to theater.

--- classes/Card.js
+++ classes/Card.js
@@ -1,11 +1,13 @@
 export default class Card {
     static id = 1;
 
-    constructor(type, strength, tacticalAbility) {
+    constructor(theater, strength, tacticalAbility, type, description) {
         this.id = Card.id++;
-        this.type = type;
+        this.theater = theater;
         this.strength = strength;
         this.tacticalAbility = tacticalAbility;
+        this.type = type;
+        this.description = description;
         this.facedown = false;
         this.covered = false;
     }
