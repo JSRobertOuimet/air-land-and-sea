44f96a013539d966745e105ab7534b62b21f1ede
JSRobertOuimet <jsrobertouimet@gmail.com>
2024-01-07 21:27:17 -0500

    Assigned player creation to game instance.

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-import Player from "./classes/Player.js";
 import Game from "./classes/Game.js";
 
-// GAME SETUP //
-const playerOne = new Player("Allison");
-const playerTwo = new Player("JS");
-const game = new Game(playerOne, playerTwo);
+const game = new Game();
 
-// // BATTLE 1 //
-// playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[0]);
-// playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[0]);
-// playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[1]);
-// playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[1]);
-// playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[2]);
-// playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[2]);
-// playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[0]);
-// playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[0]);
-// playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[1]);
-// playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[1]);
-// playerOne.improvise(playerOne.hand[0], game.battles[0].theaters[2]);
-// playerTwo.improvise(playerTwo.hand[0], game.battles[0].theaters[2]);
+// for(let player = 1; player <= 2; player++) {
+//     const playerName = prompt("What’s your name?");
 
-// console.table(game.battles[0].theaters[0].playerOneCards);
-// console.table(game.battles[0].theaters[0].playerTwoCards);
-// console.table(game.battles[0].theaters[1].playerOneCards);
-// console.table(game.battles[0].theaters[1].playerTwoCards);
-// console.table(game.battles[0].theaters[2].playerOneCards);
-// console.table(game.battles[0].theaters[2].playerTwoCards);
+//     game.createPlayer(playerName);
+// }
+
+game.createPlayer("Allison");
+game.createPlayer("Jean-Simon");
+
+game.createBattle();
+
+// console.log(game);
+console.table(game.players[0].hand);
+// console.table(game.players[1].hand);
