19489100fb6fdfd8462c1707c4e5df1aff835238
JSRobertOuimet <jsrobertouimet@gmail.com>
2024-01-13 17:27:08 -0500

    Removed commented code.

--- app.js
+++ app.js
@@ -2,12 +2,6 @@ import Game from "./classes/Game.js";
 
 const game = new Game();
 
-// for(let player = 1; player <= 2; player++) {
-//     const playerName = prompt("What’s your name?");
-
-//     game.createPlayer(playerName);
-// }
-
 game.createPlayer("Allison");
 game.createPlayer("Jean-Simon");
 
