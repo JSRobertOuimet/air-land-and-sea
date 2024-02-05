6d3f9141054ae1fb1580dfb8c0c3968f599e4657
JSRobertOuimet <jsrobertouimet@gmail.com>
2024-02-04 22:28:08 -0500

    Migrated event listener needed to create new game to App class.

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,4 @@
 import App from "./classes/App.js";
-import UI from "./classes/UI.js";
 
 const playerName = "Franklin D. Roosevelt";
 const app = new App(playerName);
-
-app.createGame();
-
-UI.nextGameButtonEl.addEventListener("click", () => {
-    UI.overlayEl.style.display = "none";
-    UI.mainAreaEl.innerHTML = "";
-    UI.playerOneHandEl.innerHTML = "";
-    UI.playerTwoHandEl.innerHTML = "";
-    app.createGame();
-});
--- scripts/classes/App.js
+++ scripts/classes/App.js
@@ -1,12 +1,30 @@
 import Game from "./Game.js";
+import UI from "./UI.js";
 
 export default class App {
     constructor(playerName) {
         this.playerName = playerName;
         this.games = [];
+
+        this.#initializeApp();
     }
 
-    createGame() {
+    #initializeApp() {
+        this.#addEventListners();
+        this.#createGame();
+    }
+
+    #addEventListners() {
+        UI.nextGameButtonEl.addEventListener("click", () => {
+            UI.overlayEl.style.display = "none";
+            UI.mainAreaEl.innerHTML = "";
+            UI.playerOneHandEl.innerHTML = "";
+            UI.playerTwoHandEl.innerHTML = "";
+            this.#createGame();
+        });
+    }
+
+    #createGame() {
         this.games.push(new Game(this));
     }
 }
