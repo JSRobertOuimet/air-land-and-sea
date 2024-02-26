90cc537318e133e68060aa620593deeef91d8c57
JSRobertOuimet <jsrobertouimet@gmail.com>
2024-02-25 21:53:25 -0500

    Added theaters and players to Log.

--- scripts/classes/Log.js
+++ scripts/classes/Log.js
@@ -1,9 +1,11 @@
 export default class Log {
-    constructor(activePlayer, selectedCard, selectedAction, selectedTheater) {
+    constructor(activePlayer, selectedCard, selectedAction, selectedTheater, players, theaters) {
         this.activePlayer = activePlayer;
         this.selectedCard = selectedCard;
         this.selectedAction = selectedAction;
         this.selectedTheater = selectedTheater;
         this.time = new Date().toLocaleTimeString();
+        this.players = players;
+        this.theaters = theaters;
     }
 }
