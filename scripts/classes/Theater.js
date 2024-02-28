849236aadb3d1735b0ba780f7a1aaea8029dcd92
JSRobertOuimet <jsrobertouimet@gmail.com>
2024-02-27 21:07:47 -0500

    Renamed properties; refactored calculatePlayerScore() method.

--- scripts/classes/Theater.js
+++ scripts/classes/Theater.js
@@ -1,5 +1,3 @@
-import Player from "./Player.js";
-
 export default class Theater {
     static id = 1;
 
@@ -7,51 +5,31 @@ export default class Theater {
         this.id = (Theater.id++).toString();
         this.name = name;
         this.playerOneCards = [];
-        this.playerOnePoints = 0;
-        this.playerOneAdditionalPoints = [];
+        this.playerOneBonus = [];
+        this.playerOneScore = 0;
         this.playerTwoCards = [];
-        this.playerTwoPoints = 0;
-        this.playerTwoAdditionalPoints = [];
+        this.playerTwoBonus = [];
+        this.playerTwoScore = 0;
     }
 
-    calculatePlayerScore(player) {
-        debugger;
-        let points = [];
-        let subtotal = 0;
+    calculatePlayerScore(playerID) {
+        const cardStrength = card => (card.facedown ? card.improviseStrength : card.deployStrength);
+        const cardPoints = this.#getCardsForPlayer(playerID).map(cardStrength);
+        const subtotal = cardPoints.reduce((sum, point) => sum + point, 0);
+        const additionalPoints = this.#getAdditionalPointsForPlayer(playerID);
+        const total =
+            playerID === "1"
+                ? (this.playerOneScore = subtotal + additionalPoints.reduce((sum, point) => sum + point, 0))
+                : (this.playerTwoScore = subtotal + additionalPoints.reduce((sum, point) => sum + point, 0));
 
-        switch (player) {
-            case "playerOne":
-                if (this.playerOneCards.length === 0) return 0;
-
-                this.playerOneCards.forEach(playerOneCard => {
-                    if (playerOneCard.facedown) {
-                        points.push(playerOneCard.improviseStrength);
-                    } else {
-                        points.push(playerOneCard.deployStrength);
-                    }
-                });
-
-                subtotal = this.#calculateSubtotal(points);
-
-                return this.playerOneAdditionalPoints.reduce((accumulator, current) => accumulator + current, subtotal);
-            case "playerTwo":
-                if (this.playerTwoCards.length === 0) return 0;
-
-                this.playerTwoCards.forEach(playerTwoCard => {
-                    if (playerTwoCard.facedown) {
-                        points.push(playerTwoCard.improviseStrength);
-                    } else {
-                        points.push(playerTwoCard.deployStrength);
-                    }
-                });
-
-                subtotal = this.#calculateSubtotal(points);
-
-                return this.playerTwoAdditionalPoints.reduce((accumulator, current) => accumulator + current, subtotal);
-        }
+        return total;
     }
 
-    #calculateSubtotal(points) {
-        return points.reduce((accumulator, current) => accumulator + current, 0);
+    #getCardsForPlayer(playerID) {
+        return playerID === "1" ? this.playerOneCards : this.playerTwoCards;
+    }
+
+    #getAdditionalPointsForPlayer(playerID) {
+        return playerID === "1" ? this.playerOneBonus : this.playerTwoBonus;
     }
 }
